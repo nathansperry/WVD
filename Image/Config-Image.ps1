0eceddc9d0af37dedce6b1f7761b9f160ff5cfae
nathansperry <nathan.sperry@virtualengine.co.uk>
2020-04-30 14:23:03 +0100

    Update Config-Image.ps1

--- Image/Config-Image.ps1
+++ Image/Config-Image.ps1
@@ -1,3 +1,54 @@
+function Start-DownloadCtxOptimiser {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [System.String] $download,
+
+        [Parameter(Mandatory)]
+        [System.String] $extractPath
+
+    )
+
+    #Downloads Latest Citrix Opimizer from https://support.citrix.com/article/CTX224676
+    #Can be used as part of a pipeline or MDT task sequence.
+    #Ryan Butler TechDrabble.com @ryan_c_butler 07/19/2019
+
+    #Uncomment to use plain text or env variables
+    $CitrixUserName = $env:ctxuser
+    $CitrixPassword = $env:ctxpw
+
+    #Uncomment to use credential object
+    #$creds = Get-Credential
+    #$CitrixUserName = $creds.UserName
+    #$CitrixPassword = $creds.GetNetworkCredential().Password
+
+    $name = ($download -split '/')[-1]
+    $extractFile = (Join-Path -path $extractPath -ChildPath $name)
+
+    #Initialize Session 
+    Invoke-WebRequest "https://identity.citrix.com/Utility/STS/Sign-In" -SessionVariable websession -UseBasicParsing
+
+    #Set Form
+    $form = @{
+        "persistent" = "1"
+        "userName" = $CitrixUserName
+        "loginbtn" = ""
+        "password" = $CitrixPassword
+        "returnURL" = "https://www.citrix.com/login/bridge?url=https%3A%2F%2Fsupport.citrix.com%2Farticle%2FCTX224676%3Fdownload"
+        "errorURL" = 'https://www.citrix.com/login?url=https%3A%2F%2Fsupport.citrix.com%2Farticle%2FCTX224676%3Fdownload&err=y'
+    }
+    #Authenticate
+    Invoke-WebRequest -Uri ("https://identity.citrix.com/Utility/STS/Sign-In") -WebSession $websession -Method POST -Body $form -ContentType "application/x-www-form-urlencoded" -UseBasicParsing
+
+    #Download File
+    Invoke-WebRequest -WebSession $websession -Uri $download -OutFile $extractFile -Verbose -UseBasicParsing
+
+    #extract file
+    Start-ExtractFile -filePath $extractFile -extractedPath (Join-Path -path $extractPath -ChildPath 'CitrixOptimizer')
+
+}
+
 function Set-Registry {
 
     [CmdletBinding()]
@@ -99,7 +150,7 @@ function Start-Download {
     }    
 }
 
-function Extract-File {
+function Start-ExtractFile {
     [CmdletBinding()]
     param (
         # Path to process to start.
@@ -116,6 +167,7 @@ function Extract-File {
  
 }
 
+
 Start-Transcript -Path  "$env:windir\Logs\Config-Image.log" -Force | Out-Null
 
 ## path to extract all files
@@ -129,10 +181,10 @@ $ukregionXML = (Join-Path -path $extractPath -ChildPath "WVD-master\Image\UKRegi
 
 ## download configuration files
 Start-Download -source $download -destination $extractFile -Verbose
-Extract-File -filePath $extractFile -extractedPath $extractPath
+Start-ExtractFile -filePath $extractFile -extractedPath $extractPath
 
 ## Set Locale, language etc. 
-& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:$ukregionXML"
+& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:"""$ukregionXML""""
 # Set Timezone
 & tzutil /s "GMT Standard Time"
 # Set languages/culture
@@ -157,15 +209,40 @@ $download = "https://statics.teams.cdn.office.net/production-windows-x64/1.3.00.
 $name = ($download -split '/')[-1]
 $extractFile = (Join-Path -path $extractPath -ChildPath $name)
 $installer = "$extractPath" + "$name"
-$arguments = "/i $installer /l`*v C:\windows\logs\teamsinstall.log ALLUSER=1 /quiet"
+$arguments = "/i $installer /l`*v C:\windows\logs\teamsinstall.log ALLUSER=1 ALLUSERS=1 /quiet"
 
 ## download and install Teams
 Start-Download -source $download -destination $extractFile -Verbose
 ## set registry to allow machine wide install
 Set-Registry -keyPath "HKLM:SOFTWARE\Microsoft\Teams" -regName IsWVDEnvironment -regValue 1 -propertyType DWord
-## set registry to stop prompt when launching from browser
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run32 -Name Teams -PropertyType Binary -Value ([byte[]](0x01,0x00,0x00,0x00,0x1a,0x19,0xc3,0xb9,0x62,0x69,0xd5,0x01)) -Force | Out-Null
+## remove from run command to stop teams auto-starting
+Remove-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Run -Name Teams -Force
 ## start install
 Start-Install -FilePath 'C:\Windows\System32\msiexec.exe' -Arguments $arguments
+
+
+## edge
+$download = "http://dl.delivery.mp.microsoft.com/filestreamingservice/files/b66ab30d-0d70-4efe-8764-b5ae8a661e1b/MicrosoftEdgeEnterpriseX64.msi"
+$name = ($download -split '/')[-1]
+$extractFile = (Join-Path -path $extractPath -ChildPath $name)
+$installer = "$extractPath" + "$name"
+$arguments = "/i $installer /l`*v C:\windows\logs\edgeinstall.log ALLUSERS=1 DONOTCREATEDESKTOPSHORTCUT=true /norestart /quiet"
+
+## download and install edge
+Start-Download -source $download -destination $extractFile -Verbose
+Start-Install -FilePath 'C:\Windows\System32\msiexec.exe' -Arguments $arguments
+
+## download and install citrix optmiser
+$download = "https://phoenix.citrix.com/supportkc/filedownload?uri=/filedownload/CTX224676/CitrixOptimizer.zip"
+$name = ($download -split '/')[-1]
+$extractFile = (Join-Path -path $extractPath -ChildPath $name)
+$ctxscript = "$extractPath" + "CitrixOptimizer\CtxOptimizerEngine.ps1"
+$arguments = "/i $installer /l`*v C:\windows\logs\edgeinstall.log ALLUSERS=1 DONOTCREATEDESKTOPSHORTCUT=true /norestart /quiet"
+Start-DownloadCtxOptimiser -download $download -extractPath $extractPath
+
+## start script
+Set-ExecutionPolicy Bypass -Scope Process -Force
+& $ctxscript -Mode Execute -OutputXml 'C:\windows\logs\CitrixOptimizerRollback.xml'
+
 
 Stop-Transcript
